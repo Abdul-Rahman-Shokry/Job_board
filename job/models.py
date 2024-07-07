3a7e59c97b16dc2d6e2d869123e4f6a245ec2b9e
Abdul-Rahman-Shokry <abdul.rahman.shokry2004@gmail.com>
2024-07-07 18:29:53 +0200

    1st model added

--- job/models.py
+++ job/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+django model field:
+    - validation
+    - HTML widgit
+    - DB size
+'''
+class Job(models.Model): #Table
+    title = models.CharField(max_length=100) #column
