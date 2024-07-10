550a799b54b4be3441f51a31b88644475e9dd59a
Abdul-Rahman-Shokry <abdul.rahman.shokry2004@gmail.com>
2024-07-10 12:44:29 +0200

    commit

--- job/models.py
+++ job/models.py
@@ -21,6 +21,16 @@ class Job(models.Model): #Table
     description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
-    # category
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     experience = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
