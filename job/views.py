b67cd9a2a6338ee5292324f5a336db599224eb5a
Abdul-Rahman-Shokry <abdul.rahman.shokry2004@gmail.com>
2024-07-11 12:42:58 +0200

    add static and media files

--- job/views.py
+++ job/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Job
+"""
+The dot . represents the current directory or
+package. So, in this case, from .models import
+Job is importing the Job class from the models
+module that is in the same directory as the
+module where this import statement is written.
+"""
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    context = {'jobs':job_list}
+    return render(request, 'job\job_list.html', context)
+
 
 def job_detail(request, id):
-    pass
+    job_detail = Job.objects.get(id = id)
+    context = {'job' : job_detail}
+    return render(request, 'job\job_detail.html', context)
